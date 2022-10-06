6a98aeaf8cbe8e8836a3b23ee16acc10e8f81633
Neil <zguolee@foxmail.com>
2022-10-06 15:52:40 +0800

    chore: delete h5 script

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
+import Uni from '@dcloudio/vite-plugin-uni'
 import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    uni(),
+    Uni(),
+
     UnoCSS(),
   ],
 })
