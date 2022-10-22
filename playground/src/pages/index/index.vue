3d1ef8ebb8fb0bc9d3f5d689da5f414869447abc
Neil <zguolee@foxmail.com>
2022-10-22 13:21:58 +0800

    chore(deps): update

--- playground/src/pages/index/index.vue
+++ playground/src/pages/index/index.vue
@@ -20,8 +20,6 @@ const goAction = (path: string) => {
 
 <template>
   <UBasePage>
-    <BButton>BButton</BButton>
-    <CButton>CButton</CButton>
     <div class="p4 pb-safe">
       <div class="text-center" flex="~ col gap1">
         <div class="text-xl">
