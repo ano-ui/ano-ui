91989abab5373110fac6bcfd7d047d1fca85daac
Neil <zguolee@foxmail.com>
2023-01-01 15:51:44 +0800

    fix: ci

--- packages/ano-ui/vite.config.ts
+++ packages/ano-ui/vite.config.ts
@@ -12,12 +12,10 @@ export default defineConfig({
     },
     outDir: 'dist',
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
+      // make sure to externalize deps that shouldn't be bundled into your library
       external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
+        // Provide global variables to use in the UMD build for externalized deps
         globals: {
           vue: 'Vue',
         },
