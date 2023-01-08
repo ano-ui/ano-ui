fcae01c4ee6a437eb41c4d33ce1fa33667b3c5d6
Neil <zguolee@foxmail.com>
2023-01-08 21:32:44 +0800

    fix(components): `ANotify` position

--- packages/preset/src/shortcuts/notify.ts
+++ packages/preset/src/shortcuts/notify.ts
@@ -4,7 +4,7 @@ export const notifyShortcuts: Record<string, string> = {
   'a-notify-position-top': 'top-0 left-5 right-5 pt-5',
   'a-notify-position-top-left': 'top-5 left-0 pl-5',
   'a-notify-position-top-right': 'top-5 right-0 pr-5',
-  'a-notify-position-bottom': 'left-5 right-5 mb-safe pb-5',
+  'a-notify-position-bottom': 'bottom-0 left-5 right-5 mb-safe pb-5',
   'a-notify-position-bottom-left': 'bottom-5 left-5 mb-safe pl-5',
   'a-notify-position-bottom-right': 'bottom-5 right-5 mb-safe pr-5',
   'a-notify-content-base': 'flex justify-center gap2 items-center py2 px3 rounded-lg dark:bg-op80 dark:text-op80 a-transition',
