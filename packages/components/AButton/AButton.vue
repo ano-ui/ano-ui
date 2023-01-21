8dfedd5609893144d06bebe379a05304d4e5cddd
Neil <zguolee@foxmail.com>
2023-01-21 10:32:34 +0800

    styles(AButton): change hover style

--- packages/components/AButton/AButton.vue
+++ packages/components/AButton/AButton.vue
@@ -10,14 +10,15 @@ const { disabled, clickHandler } = useButton(props, emit)
 
 <template>
   <button
-    class="a-button-base"
-    :class="[`a-${type}`, { 'w-full': block }, `a-button-${size}`, { 'a-button-icon': iconOnly }, `a-${variant}`, { 'a-disabled': disabled }, cc]"
-    :hover-class="disabled ? '' : (variant === 'text' ? 'a-button-text-hover' : 'a-button-hover')" :open-type="openType"
-    :style="cs" @click="clickHandler"
+    class="a-button-wrapper-base"
+    :class="[`a-${type}`, { block }, `a-button-${size}`, { 'a-button-icon': iconOnly }, `a-${variant}`, { 'a-disabled': disabled }, cc]"
+    :hover-class="disabled ? '' : 'a-button-hover'" hover-stay-time="100" :open-type="openType" :style="cs"
+    @click="clickHandler"
   >
-    <div v-if="loading" class="i-carbon-circle-dash animate-spin" />
-    <div v-else-if="icon" :class="[icon]" />
-    <slot v-else name="icon" />
-    <slot />
+    <div class="a-button-content-base">
+      <div v-if="loading" class="i-carbon-circle-dash animate-spin" />
+      <div v-else-if="icon" class="mr-1" :class="[icon]" />
+      <slot />
+    </div>
   </button>
 </template>
--- packages/preset/src/shortcuts/base.ts
+++ packages/preset/src/shortcuts/base.ts
@@ -11,7 +11,7 @@ export const baseShortcuts: Record<string, string> = {
   'a-solid': 'bg-context border-context text-white',
   'a-outline': 'bg-context border-context !bg-op0 text-context',
   'a-ghost': 'a-outline border-dashed',
-  'a-light': 'bg-context border-context !bg-op20 !border-op0 text-context',
+  'a-light': 'bg-context border-context !bg-op20 !border-op20 text-context',
   'a-text': 'border-context bg-transparent !border-op0 text-context',
   'a-disabled': 'op70',
 }
