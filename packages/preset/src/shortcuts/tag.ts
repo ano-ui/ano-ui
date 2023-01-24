61529ef0af7b347d1dac5493fa062b614871e101
Neil <zguolee@foxmail.com>
2023-01-24 10:31:29 +0800

    styles(ATag): delete useless

--- packages/preset/src/shortcuts/tag.ts
+++ packages/preset/src/shortcuts/tag.ts
@@ -1,11 +1,9 @@
 export const tagShortcuts: Record<string, string> = {
-  // 'a-tag-base': `inline-flex justify-center gap1 items-center border border-solid a-transition
-  // dark:bg-op80 dark:border-op80 dark:text-op80 !leading-none`,
   'a-tag-base': `relative box-border inline-flex justify-center items-center gap1 after:display-none a-transition overflow-visible bg-clip-padding
    before:content-none before:absolute before:w-full before:h-full before:left-50% before:top-50% before:-translate-50% 
    before:border-style-inherit before:border-width-inherit before:border-rd-inherit before:border-black before:bg-black before:op0`,
   'a-tag-disabled': 'before:display-none op50',
-  'a-tag-mini': 'px1 ph-5 text-xs',
+  'a-tag-mini': 'px1 h-5 text-xs',
   'a-tag-small': 'px1.5 h-6 text-sm',
   'a-tag-normal': 'px2 h-7 text-base',
   'a-tag-large': 'px2.5 h-8 text-lg',
