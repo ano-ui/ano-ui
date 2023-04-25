630048350ef255338549a36601504a22b04375d8
Mao Mr <49724027+hairyf@users.noreply.github.com>
2023-04-25 08:34:46 +0800

    refactor: standard notify func names (#43)
    * chore: standard toast func names
    
    * chore: sync playground toast func
    
    * chore: standard notify func names

--- packages/ano-ui/src/components/ANotify/ANotify.vue
+++ packages/ano-ui/src/components/ANotify/ANotify.vue
@@ -7,9 +7,9 @@ import { useNotify } from './use-notify'
 const props = defineProps(notifyProps)
 const emit = defineEmits(notifyEmits)
 
-const { showValue, notifyStatus, showNotify, closeNotify } = useNotify(props, emit)
+const { visible, state, show, close } = useNotify(props, emit)
 
-defineExpose({ showNotify, closeNotify })
+defineExpose({ show, close })
 
 const animationName = {
   'default': 'slide-down',
@@ -24,31 +24,31 @@ const animationName = {
 
 <template>
   <ATransition
-    :show="showValue" :name="animationName[notifyStatus?.position || 'default']" :duration="300"
-    :cc="['a-notify-base', `a-notify-position-${notifyStatus.position}`, cc]" :cs="cs"
+    :show="visible" :name="animationName[state?.position || 'default']" :duration="300"
+    :cc="['a-notify-base', `a-notify-position-${state.position}`, cc]" :cs="cs"
   >
     <div
       class="a-notify-content-base"
-      :class="[`a-${notifyStatus.type}`, { 'rounded-none': notifyStatus.position === 'default' }, { 'justify-start': notifyStatus.showIcon }, ccc]"
+      :class="[`a-${state.type}`, { 'rounded-none': state.position === 'default' }, { 'justify-start': state.showIcon }, ccc]"
       :style="ccs"
     >
-      <template v-if="notifyStatus.customIcon">
+      <template v-if="state.customIcon">
         <slot name="icon" />
       </template>
 
-      <template v-else-if="notifyStatus.showIcon">
-        <div v-if="notifyStatus.type === 'primary'" class="i-tabler-info-circle-filled" />
-        <div v-else-if="notifyStatus.type === 'success'" class="i-tabler-circle-check-filled" />
-        <div v-else-if="notifyStatus.type === 'warning'" class="i-tabler-alert-circle-filled" />
-        <div v-else-if="notifyStatus.type === 'danger'" class="i-tabler-circle-x-filled" />
+      <template v-else-if="state.showIcon">
+        <div v-if="state.type === 'primary'" class="i-tabler-info-circle-filled" />
+        <div v-else-if="state.type === 'success'" class="i-tabler-circle-check-filled" />
+        <div v-else-if="state.type === 'warning'" class="i-tabler-alert-circle-filled" />
+        <div v-else-if="state.type === 'danger'" class="i-tabler-circle-x-filled" />
       </template>
-      <div class="flex-1" :class="{ 'text-center': !notifyStatus.showIcon && !notifyStatus.customIcon }">
-        <template v-if="notifyStatus.message">
-          {{ notifyStatus.message }}
+      <div class="flex-1" :class="{ 'text-center': !state.showIcon && !state.customIcon }">
+        <template v-if="state.message">
+          {{ state.message }}
         </template>
         <slot v-else />
       </div>
-      <div v-if="notifyStatus.showIcon || notifyStatus.showClose" class="i-tabler-x" @click.stop="closeNotify" />
+      <div v-if="state.showIcon || state.showClose" class="i-tabler-x" @click.stop="close" />
     </div>
   </ATransition>
 </template>
