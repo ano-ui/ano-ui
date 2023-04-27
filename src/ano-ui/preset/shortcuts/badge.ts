71cbecaecf5a3553851ce57a1a4d18c2110324b2
Neil <zguolee@foxmail.com>
2023-04-27 09:36:34 +0800

    chore: styles build

--- src/ano-ui/preset/shortcuts/badge.ts
+++ src/ano-ui/preset/shortcuts/badge.ts
@@ -1,6 +1,6 @@
 export const badgeShortcuts: Record<string, string> = {
   'a-badge-base': 'inline-block relative text-xs',
-  'a-badge-sup': 'absolute inline-block border-box rounded-full text-white left-full z-10 bg-context whitespace-nowrap',
+  'a-badge-sup': 'absolute inline-block box-border rounded-full text-white left-full z-10 bg-context whitespace-nowrap',
   'a-badge-s-dot': 'w-16rpx h-16rpx',
   'a-badge-s-default': 'h-36rpx px-2',
   'a-badge-wave': 'z-1 absolute inset-0 rounded-inherit',
