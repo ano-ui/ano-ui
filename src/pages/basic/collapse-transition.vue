c986a1ee69c8ab1431b546f8b99219164e0974ba
Mr.Mao <951416545@qq.com>
2023-04-29 19:36:42 +0800

    feat(component): collapse-transition example

---
+++ src/pages/basic/collapse-transition.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const checked = ref(true)
+</script>
+
+<template>
+  <UBasePage>
+    <div class="p-4">
+      Basic
+    </div>
+    <div flex="~ gap2 wrap" items-center px-4>
+      <ASwitch v-model="checked" />
+    </div>
+    <div class="p-4">
+      <ACollapseTransition :show="checked">
+        宿建德江移舟泊烟渚，日暮客愁新。野旷天低树，江清月近人。
+      </ACollapseTransition>
+    </div>
+  </UBasePage>
+</template>
+
+<route lang="yaml">
+style:
+  navigationBarTitleText: CollapseTransition 折叠过渡
+</route>
