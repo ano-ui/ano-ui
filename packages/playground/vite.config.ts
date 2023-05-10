6f49266633010dc191f847a39de40690c4591ae5
kejun <kejun1997@gmail.com>
2023-05-10 23:09:06 +0800

    feat: use vite-plugin-uni-manifest and minify uniapp config

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -5,6 +5,8 @@ import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from '@uni-helper/vite-plugin-uni-components'
 import UniPages from '@uni-helper/vite-plugin-uni-pages'
+import UniManifest from '@uni-helper/vite-plugin-uni-manifest'
+
 import { AnoDevResolver } from './resolvers'
 
 export default defineConfig({
@@ -20,7 +22,11 @@ export default defineConfig({
       dts: 'src/components.d.ts',
       resolvers: [AnoDevResolver(process.env.NODE_ENV === 'development' ? 'serve' : 'build')],
     }),
-    UniPages({ routeBlockLang: 'yaml' }),
+    UniPages({
+      routeBlockLang: 'yaml',
+      minify: true,
+    }),
+    UniManifest({ minify: true }),
     Uni(),
 
     // https://github.com/antfu/unocss
